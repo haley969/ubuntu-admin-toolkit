9f98f0a134577e551a877f6cd9bec8780f25b207
ネプギアGO <8287084+nepgeargo@users.noreply.github.com>
2020-11-02 20:49:53 +1100

    Add files via upload

--- install-mariadb10.5-bionic.sh
+++ install-mariadb10.5-bionic.sh
@@ -1,3 +1,7 @@
+echo
+echo "Installing MariaDB"
+echo
+
 sudo apt install software-properties-common -y
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mariadb.mirror.digitalpacific.com.au/repo/10.5/ubuntu bionic main'
--- install-mariadb10.5-focal.sh
+++ install-mariadb10.5-focal.sh
@@ -1,6 +1,10 @@
+echo
+echo "Installing MariaDB"
+echo
+
 sudo apt-get install software-properties-common -y
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirror.rackspace.com/mariadb/repo/10.5/ubuntu focal main'
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mariadb.mirror.digitalpacific.com.au/repo/10.5/ubuntu focal main'
 sudo apt update
 sudo apt install mariadb-server -y
 
